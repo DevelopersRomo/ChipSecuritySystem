a4ebd843db905b7a254f3b9a6f6c397da3aae35a
Devromo <jony_romo@hotmail.com>
2025-04-28 15:10:13 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ChipSecuritySystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Example from the problem statement
+            List<ColorChip> exampleChips = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Yellow, Color.Red),
+                new ColorChip(Color.Orange, Color.Purple)
+            };
+
+            RunTest("Example Test", exampleChips);
+
+            // Additional test case with no solution
+            List<ColorChip> noSolutionChips = new List<ColorChip>
+            {
+                new ColorChip(Color.Red, Color.Yellow),
+                new ColorChip(Color.Yellow, Color.Purple),
+                new ColorChip(Color.Purple, Color.Orange)
+            };
+
+            RunTest("No Solution Test", noSolutionChips);
+
+            // Additional test case with multiple possible paths
+            List<ColorChip> multiplePathsChips = new List<ColorChip>
+            {
+                new ColorChip(Color.Blue, Color.Red),
+                new ColorChip(Color.Blue, Color.Yellow),
+                new ColorChip(Color.Yellow, Color.Green),
+                new ColorChip(Color.Red, Color.Green),
+                new ColorChip(Color.Red, Color.Yellow)
+            };
+
+            RunTest("Multiple Paths Test", multiplePathsChips);
+
+            // Wait for user input before closing
+            Console.WriteLine("\nPress any key to exit...");
+            Console.ReadKey();
+        }
+
+        static void RunTest(string testName, List<ColorChip> chips)
+        {
+            Console.WriteLine($"\n=== {testName} ===");
+            Console.WriteLine("Available Chips:");
+            foreach (var chip in chips)
+            {
+                Console.WriteLine($"[{chip.StartColor}, {chip.EndColor}]");
+            }
+
+            SecuritySystem securitySystem = new SecuritySystem();
+            List<ColorChip> sequence = securitySystem.FindLongestValidSequence(chips);
+
+            Console.WriteLine("\nResult:");
+            if (sequence != null && sequence.Count > 0)
+            {
+                Console.WriteLine(securitySystem.FormatSequence(sequence));
+                Console.WriteLine($"Used {sequence.Count} out of {chips.Count} chips");
+                Console.WriteLine($"Valid sequence: {securitySystem.ValidateSequence(sequence)}");
+            }
+            else
+            {
+                Console.WriteLine(Constants.ErrorMessage);
+            }
+        }
+    }
+}
---
+++ SecuritySystem.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChipSecuritySystem
+{
+    public class SecuritySystem
+    {
+        public List<ColorChip> FindLongestValidSequence(List<ColorChip> chips)
+        {
+            List<ColorChip> bestSequence = new List<ColorChip>();
+            List<ColorChip> currentSequence = new List<ColorChip>();
+            bool[] used = new bool[chips.Count];
+
+            DFS(Color.Blue, chips, used, currentSequence, ref bestSequence);
+
+            return bestSequence.Count > 0 ? bestSequence : null;
+        }
+
+        private void DFS(Color currentColor, List<ColorChip> chips, bool[] used, List<ColorChip> currentSequence, ref List<ColorChip> bestSequence)
+        {
+            // Check if current sequence ends in Green and is better
+            if (currentColor == Color.Green)
+            {
+                if (currentSequence.Count > bestSequence.Count)
+                {
+                    bestSequence = new List<ColorChip>(currentSequence);
+                }
+                return;
+            }
+
+            for (int i = 0; i < chips.Count; i++)
+            {
+                if (!used[i])
+                {
+                    var chip = chips[i];
+
+                    if (chip.StartColor == currentColor)
+                    {
+                        used[i] = true;
+                        currentSequence.Add(chip);
+                        DFS(chip.EndColor, chips, used, currentSequence, ref bestSequence);
+                        currentSequence.RemoveAt(currentSequence.Count - 1);
+                        used[i] = false;
+                    }
+                    else if (chip.EndColor == currentColor)
+                    {
+                        // Flip the chip
+                        var flippedChip = new ColorChip(chip.EndColor, chip.StartColor);
+                        used[i] = true;
+                        currentSequence.Add(flippedChip);
+                        DFS(flippedChip.EndColor, chips, used, currentSequence, ref bestSequence);
+                        currentSequence.RemoveAt(currentSequence.Count - 1);
+                        used[i] = false;
+                    }
+                }
+            }
+        }
+
+        public string FormatSequence(List<ColorChip> sequence)
+        {
+            List<string> formatted = new List<string> { "Blue" };
+
+            foreach (var chip in sequence)
+            {
+                formatted.Add($"[{chip.StartColor}, {chip.EndColor}]");
+            }
+
+            formatted.Add("Green");
+
+            return string.Join(" -> ", formatted);
+        }
+
+        public bool ValidateSequence(List<ColorChip> sequence)
+        {
+            if (sequence == null || sequence.Count == 0)
+                return false;
+
+            Color expectedColor = Color.Blue;
+
+            foreach (var chip in sequence)
+            {
+                if (chip.StartColor != expectedColor)
+                    return false;
+
+                expectedColor = chip.EndColor;
+            }
+
+            return expectedColor == Color.Green;
+        }
+    }
+}
